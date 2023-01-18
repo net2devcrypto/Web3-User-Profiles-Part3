6dee9b9940b423cbd039e24d6e7e2698ca55abf3
Net2Dev <info@a3b.io>
2023-01-17 17:17:57 -0800

    Update config.js

--- components/config.js
+++ components/config.js
@@ -2,10 +2,12 @@ import { ethers } from "ethers";
 import { create as ipfsHttpClient } from "ipfs-http-client";
 import userDbAbi from '../components/userdb.json';
 
-export const client = ipfsHttpClient('http://10.10.20.32:5001');
-export const userdbaddress = '0xA146575395D7f685CC096A227f895137A1517FFE';
-const rpc = 'https://rpc.ankr.com/polygon_mumbai';
-const updaterwallet = '89b0896f8b0fea1cec2d7dfd0204ba63e896d4b58581d299aea477dc3f2b2a35';
+
+export const client = ipfsHttpClient('http://IP-ADDRESS-OF-YOUR-IPFS-NODE:5001');
+export const userdbaddress = '0xSMART-CONTRACT-ADDRESS-FROM-STEP-5';
+const rpc = 'ADDRESS OF TESTNET DEPLOYED';
+const updaterwallet = 'KEY-OBTAINED-FROM-STEP-6';
 const provider = new ethers.providers.JsonRpcProvider(rpc);
 const updater = new ethers.Wallet(updaterwallet, provider);
 export const usercontract = new ethers.Contract(userdbaddress, userDbAbi, updater);
+
